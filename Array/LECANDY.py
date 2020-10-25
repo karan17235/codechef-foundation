38f7d9dbe08e0274188d32e7e3694cfd1f67c722
Karan Bansal <1karanbansal@gmail.com>
2020-10-26 00:36:07 +1100

    Adding array programs for codechef practice

--- Array/LECANDY.py
+++ Array/LECANDY.py
@@ -3,7 +3,7 @@
 try:
     for _ in range(int(input())):
         number ,total_candies = map(int,input().split())
-        candy_distribution = map(int,input().split())))
+        candy_distribution = map(int,input().split())
         if total_candies < sum(candy_distribution):
             print("No")
         else:
